e8187fbd2af2b168b63b980e2f0673e18dfde8c5
NZSmartie <nzsmartie@gmail.com>
2018-10-04 14:51:01 +1300

    loop through rc files and set correct ownership

--- setup.sh
+++ setup.sh
@@ -8,15 +8,23 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 cp .startup.sh $HOME/.startup.sh
-chown root $HOME/.startup.sh
+chown root:$SUDO_GID $HOME/.startup.sh
 chmod go-w,a+x $HOME/.startup.sh
 
 echo "Please add the following entry to sudoers via visudo"
 echo "$SUDO_USER ALL=(root) NOPASSWD: $HOME/.startup.sh"
 
-cp $DIR/.gitconfig $HOME/.gitconfig
-cp $DIR/.pythonrc $HOME/.pythonrc
-cp $DIR/.screenrc $HOME/.screenrc
-cp $DIR/.vimrc $HOME/.vimrc
-cp .bash_profile $HOME/.bash_profile
+declare -a RCFILES=(
+    .gitconfig
+    .pythonrc
+    .screenrc
+    .vimrc
+    .bash_profile
+)
+
+for rcfile in "${RCFILES[@]}"
+do
+    cp $DIR/$rcfile $HOME/
+    chown $SUDO_UID:$SUDO_GID $HOME/$rcfile
+done
 
